dfdb5b23084dbe1db1495b43bff3f3ea484bb873
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-18 19:03:34 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,10 @@
 def my_select(collection)
   i = 0 
-  selected = []
-  while i < collection.size
-    yield collection[i]
-    i += 1 
-  end
-  selected
+  if block_given? 
+    selected = []
+    while i < collection.size
+      yield collection[i]
+      i += 1 
+    end
+    selected
 end
